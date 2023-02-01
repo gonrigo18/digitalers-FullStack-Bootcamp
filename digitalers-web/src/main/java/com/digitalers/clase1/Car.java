7f5207471c298aa7dcfe372956a3e4d5451a4d1d
Gonzalo Rigobello <gonzalo.rigobello18@gmail.com>
2023-02-01 20:27:55 -0300

    Class Car

---
+++ digitalers-web/src/main/java/com/digitalers/clase1/Car.java
@@ -0,0 +1,14 @@
+package com.digitalers.clase1;
+
+public class Auto {
+	
+	String brand;
+	String color;
+	String model;
+	Integer chasis;
+	Float maxVelocity;
+	Float currentSpeed;
+	Boolean newModel;
+	
+
+}
